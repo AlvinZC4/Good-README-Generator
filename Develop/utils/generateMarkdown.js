66c7714f2850cb3c28676510be839e0b6f523f09
Alvin <alvinzcoxiv@gmail.com>
2020-07-24 23:54:45 -0400

    Began constructing skeleton of generates README...still work to do on skeleton and to add to inquier prompts

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,45 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${data.description}
+
+  ***
+
+  ### Table of Contents:
+
+  1. [How to Install ${data.title}](#Installation)
+  2. [Using ${data.title}](#How%20To%20Use%20${data.title})
+  3. [Testing ${data.title}](#Test%20Code)
+  4. [Contributors](#Contributors)
+  5. [Ask Us Questions](#Ask%20Questions)
+  6. [Licenses](#License)
+
+  ***
+
+  # Installation
+
+  ${install}
+
+  # How to Use ${data.title}
+
+  ${usage}
+
+  # Test Code
+
+  ${data.tests}
+
+  # Contributors
+
+  ${data.contribute}
+
+  # Ask Questions
+
+  ${data.questions}
+
+  # License
+
+  ${data.license}
+
 `;
 }
 
